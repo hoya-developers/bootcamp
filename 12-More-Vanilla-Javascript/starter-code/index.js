f603eb0f1380d548a2d1f6120efb6f323f1dc8bf
Reed Uhlik <reeduhlik@gmail.com>
2024-10-30 14:57:12 -0400

    lecture 12 starter code

---
+++ 12-More-Vanilla-Javascript/starter-code/index.js
@@ -0,0 +1,17 @@
+let todos = [];
+
+function addTodo() {
+  //To-Do
+}
+
+function toggleTodo(index) {
+  //To-Do
+}
+
+function deleteTodo(index) {
+  //To-Do
+}
+
+function renderTodos() {
+  //To-Do
+}
